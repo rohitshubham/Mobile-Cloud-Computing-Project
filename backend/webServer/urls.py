b2c32678407b83a485f5e7ecda1880f398303c06
Shuvo Kibria <kibria.shuvo@aalto.fi>
2019-11-12 23:21:28 +0200

    Req updated

--- backend/webServer/urls.py
+++ backend/webServer/urls.py
@@ -1,17 +1,7 @@
 from django.contrib import admin
-<<<<<<< HEAD
-from django.conf.urls import url 
-from . import views
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$',views.singIn),
-    url(r'^postsign/',views.postsign),
-]
-=======
 from django.urls import path, include
 
 urlpatterns = [
     path('project/', include('mcc.urls')),
     path('admin/', admin.site.urls),
 ]
->>>>>>> origin/master
