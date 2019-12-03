b77b9fcc6c6fd137034e07edb458f22a9c997d24
Rohit Raj <rohit.raj@aalto.fi>
2019-12-04 00:05:35 +0200

    Create a typeaheadService

--- backend/mcc/urls.py
+++ backend/mcc/urls.py
@@ -6,10 +6,11 @@ from . import views
 urlpatterns = [
     path('user/', views.user_save, name=''),
     path('user/<str:email_id>', views.user_get, name=""),
+    path('project/user/<str:search_path>', views.user_typeahead, name = ""),
     path('project/', views.project_save, name=''),
     path('project/<str:project_id>', views.project_details, name=''),
     path('projects/<str:email_id>', views.projects_list, name=''),
-    path('project/generate_report/<str:project_id>',views.generate_report,name=''),
+    path('project/report/<str:project_id>',views.generate_report,name=''),
     path('task/',views.task_save,name=''),
     path('task/member/',views.add_member_to_task ,name=''),
     path('task/complete/', views.set_task_completed, name='')
