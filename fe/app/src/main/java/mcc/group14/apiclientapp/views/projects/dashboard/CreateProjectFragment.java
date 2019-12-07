d837fe107a45101709334f51944c809029c4ad7d
Kibria Shuvo <kibria.shuvo@aalto.fi>
2019-12-07 02:35:04 +0200

    Added project create view

--- fe/app/src/main/java/mcc/group14/apiclientapp/views/projects/dashboard/CreateProjectFragment.java
+++ fe/app/src/main/java/mcc/group14/apiclientapp/views/projects/dashboard/CreateProjectFragment.java
@@ -1,19 +1,102 @@
 package mcc.group14.apiclientapp.views.projects.dashboard;
 
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.hootsuite.nachos.NachoTextView;
+import com.hootsuite.nachos.terminator.ChipTerminatorHandler;
 
 import mcc.group14.apiclientapp.R;
 
 public class CreateProjectFragment extends Fragment {
 
+    private Context mContext;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mContext = context;
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mContext = null;
+    }
+
+
+    DatePickerDialog picker;
+
+    EditText txtDeadline;
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_user_settings, container, false);
+        View view = inflater.inflate(R.layout.fragment_create_project, container, false);
+
+        Spinner spinner = (Spinner) view.findViewById(R.id.projects_spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(mContext,
+                R.array.project_type, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+
+
+        spinner.setAdapter(adapter);
+        spinner.setPrompt("Personal");
+
+        //=================Chip
+
+        NachoTextView nachoTextView;
+
+        nachoTextView = (NachoTextView) view.findViewById(R.id.nacho_keywords);
+
+        nachoTextView.addChipTerminator(' ', ChipTerminatorHandler.BEHAVIOR_CHIPIFY_ALL);
+
+
+
+
+        //======Date picker===================
+
+        txtDeadline=(EditText) view.findViewById(R.id.txtdeadline);
+        txtDeadline.setInputType(InputType.TYPE_NULL);
+        txtDeadline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                picker = new DatePickerDialog(mContext,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                txtDeadline.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                            }
+                        }, year, month, day);
+                picker.show();
+            }
+        });
+
+        //==============================
+
+        return view;
+
+
     }
 }
